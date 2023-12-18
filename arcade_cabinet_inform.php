91c471397166ae663308060c0a9f68e7b2389236
R1Vix <90548135+R1Vix@users.noreply.github.com>
2023-12-18 21:10:43 +0800

    Add files via upload

---
+++ arcade_cabinet_inform.php
@@ -0,0 +1,26 @@
+<?php
+    header("Content-Type: text/html; charset=utf-8");
+    include_once "arcade_connect.php";
+    echo "<table border = '1'>
+    <tr>
+    <th>店名</th>
+    <th>機台名稱</th>
+    <th>排隊方式</th>
+    <th>投幣方式</th>
+    </tr>";
+
+    $query = ("select * from cabinet order by ID asc");
+    $stmt = $db -> prepare($query);
+    $stmt -> execute();
+    $result = $stmt -> fetchAll();
+    for($i = 0; $i < count($result); $i++){
+        echo "<tr>";
+        echo "<td>".$result[$i]['storeName']."</td>";
+        echo "<td>".$result[$i]['cabinetName']."</td>";
+        echo "<td>".$result[$i]['wait']."</td>";
+        echo "<td>".$result[$i]['price']."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "<br><input type = 'button' onclick = 'history.back()' value = '回到前一頁'></input>";
+?>
---
+++ arcade_cabinet_update.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "arcade_connect.php";
+    $storeName = $_POST["storeName"];
+    $cabinetName = $_POST["cabinetName"];
+    $wait = $_POST["wait"];
+    $price = $_POST["price"];
+    $query = ("update cabinet set wait = ?, price = ? where storeName = ? and cabinetName = ?");
+    $stmt = $db -> prepare($query);
+    $result = $stmt -> execute(array($wait, $price, $storeName, $cabinetName));
+    header("Location: arcade_cabinet_inform.php")
+?>
