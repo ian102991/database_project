070a335c965c380cf468e4000aa500d413279fe4
ian102991 <ian102991@gmail.com>
2024-01-01 17:33:45 +0800

    add sub query and count

--- searh_date.php
+++ searh_date.php
@@ -1,9 +1,27 @@
 <?php
+try{
     include_once "db_conn.php";
     $a=$_GET["date1"];
     $b=$_GET["date2"];
     $a=new datetime($a);
     $b=new datetime($b);
+    $query = ("select *,count(*) from ((select * from homework4.playdata where UNIX_TIMESTAMP(playTime) < UNIX_TIMESTAMP(?)) except (select * from homework4.playdata where UNIX_TIMESTAMP(playTime) < UNIX_TIMESTAMP(?))) as a group by playerName");
+    $stmt= $db->prepare($query);
+    $stmt->execute(array($b->format('Y-m-d H:i:s'),$a->format('Y-m-d H:i:s')));
+    $result=$stmt->fetchAll();
+    echo "<table border='1'>
+    <tr>
+    <th>玩家名</th>
+    <th>遊玩次數</th>
+    </tr>";
+    for($i=0;$i<count($result);$i++){
+        echo "<tr>";
+        echo "<th>".$result[$i]['playerName']."</th>";
+        echo "<th>".$result[$i]['count(*)']."</th>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "<br>";
     $query = ("(select * from homework4.playdata where UNIX_TIMESTAMP(playTime) < UNIX_TIMESTAMP(?)) except (select * from homework4.playdata where UNIX_TIMESTAMP(playTime) < UNIX_TIMESTAMP(?))");
     $stmt= $db->prepare($query);
     $stmt->execute(array($b->format('Y-m-d H:i:s'),$a->format('Y-m-d H:i:s')));
@@ -25,6 +43,10 @@
         echo "<th>".$result[$i]['playTime']."</th>";
         echo "</tr>";
     }
-    echo "<table>";
+    echo "</table>";
     echo "<br><input type='button' onclick='history.back()' value='回上頁'>";
+}
+catch(PDOException $e){
+    echo "捕获到异常：" . $e->getMessage();
+}
 ?>
